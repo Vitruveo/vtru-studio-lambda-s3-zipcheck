790f25163de9a7aad88f58f245bdb534f2c45d3a
marcos <marcos.barboza@jbtec.com.br>
2024-02-06 13:47:50 -0300

    feat: post notify

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 // This function is triggered by an S3 event and checks if there are any blacklisted files in the uploaded zip file.
-
+const axios = require('axios');
 const admzip = require('adm-zip');
 const s3client = require('@aws-sdk/client-s3');
 const cryto = require('crypto');
@@ -30,16 +30,23 @@ const blackList = [
     'rar',
 ];
 
-const notify = async ({ filename, bucket, region }) => {
-    // TODO: marcos, please implement that 😆
+const notify = async ({ filename }) => {
+    const url = `${process.env.NOTIFY_API_URL}/assets/notify/file`;
+    const data = { filename };
+
+    try {
+        const response = await axios.post(url, data);
+        console.log(`Status: ${response.status}`);
+        console.log('Body: ', response.data);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
 };
 
-const md5hash = (value) => {
-    return cryto.createHash('md5').update(value).digest('hex');
-};
+const md5hash = (value) => cryto.createHash('md5').update(value).digest('hex');
 
 const checkFile = async ({ filename, bucket, region }) => {
-    const s3 = new s3client.S3Client({ region: region });
+    const s3 = new s3client.S3Client({ region });
     const params = {
         Bucket: bucket,
         Key: filename,
